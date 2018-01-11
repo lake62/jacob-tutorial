9971b9370df43d81a7abfc88fe4fe475a01afe86
lake62 <32144428+lake62@users.noreply.github.com>
2018-01-11 19:09:30 +0100

    updated the function to not remove punctuation but only remove niqqud

--- docs/js/nikkud-remover.js
+++ docs/js/nikkud-remover.js
@@ -1,22 +1,22 @@
-﻿
-function stripVowels(rawString)
-{
-	var newString = '';
-	for(j=0; j<rawString.length; j++) {
-		if(rawString.charCodeAt(j)<1425
-			 || rawString.charCodeAt(j)>1479)
-		{ newString = newString + rawString.charAt(j); }
-	}
-	return(newString);
+﻿/*
+What is Nikkud? What are the Unicodes?
+https://en.wikipedia.org/wiki/Unicode_and_HTML_for_the_Hebrew_alphabet
+https://en.wikipedia.org/wiki/Niqqud
+https://en.wikipedia.org/wiki/Ktiv_hasar_niqqud
+http://www.unicode.org/charts/PDF/U0590.pdf
+ */
+
+ var str = '#this #is__ __#a test###__'
+str = str.replace(/#|_/g,'')
+
+function removeNikkud(rawString) {
+	// exclude U+05BE , U+05C0 , U+05C3	and U+05C6
+	return rawString.replace(/[\u0591-\u05BD]|[\u05BF]|[\u05C1-\u05C2]|[\u05C4-\u05C5]|[\u05C7]/g,"")
+  //return rawString.replace(/[\u0591-\u05C7]/g,"")
 }
 
-/* @shimondoodkin suggested even a much shorter way to do this */
-function stripVowels2(rawString) {
-  return rawString.replace(/[\u0591-\u05C7]/g,"")
-}
-
-function doStrip() {
+function flattenNikkud() {
    var input = $('#input').val();
-   var output = stripVowels2(input);
+   var output = removeNikkud(input);
    $('#output').val(output);
 }
