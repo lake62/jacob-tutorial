eee366b6277a784b85597a74be2b076be1f56f18
lake62 <32144428+lake62@users.noreply.github.com>
2018-01-11 19:27:00 +0100

    roll back to previous function that removes puntuation as well

--- docs/js/nikkud-remover.js
+++ docs/js/nikkud-remover.js
@@ -6,9 +6,6 @@ https://en.wikipedia.org/wiki/Ktiv_hasar_niqqud
 http://www.unicode.org/charts/PDF/U0590.pdf
  */
 
- var str = '#this #is__ __#a test###__'
-str = str.replace(/#|_/g,'')
-
 function removeNikkud(rawString) {
 	// exclude U+05BE , U+05C0 , U+05C3	and U+05C6
 	//return rawString.replace(/[\u0591-\u05BD|\u05BF|\u05C1-\u05C2|\u05C4-\u05C5|\u05C7]/g,"")
